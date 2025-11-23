f85b6a0216189c5a2b1279a8b0ca1c08909c98c2
Gilnathan Reis <gilnathan.dev@icloud.com>
2025-11-23 15:27:54 -0300

    Autenticação
    Implementação completa da API de Produtos com Autenticação JWT

--- Program.cs
+++ Program.cs
@@ -90,83 +90,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
-app.MapGet("/test", () => "Esse é um endpoint de teste");
-
-var produtos = new List<Produto>()
-{
-    new Produto() { Id = 1, Nome = "Mouse sem fio", Preco = 99.90M, Estoque = 50 },
-    new Produto() { Id = 2, Nome = "Teclado", Preco = 249.90M, Estoque = 30 }
-};
-
-app.MapGet("/produtos", () =>
-{
-    return produtos;
-});
-
-app.MapGet("/produtos/{id}", (int id) =>
-{
-    var produto = produtos.FirstOrDefault(x => x.Id == id);
-
-    return produto is not null
-        ? Results.Ok(produto)
-        : Results.NotFound($"Produto com ID {id} não encontrado.");
-});
-
-app.MapPost("/produtos", (Produto novoProduto) =>
-{
-    produtos.Add(novoProduto);
-
-    return Results.Created();
-});
-
-app.MapPut("/produtos/{id}", (int id, Produto produtoAtualizado) =>
-{
-    var produto = produtos.FirstOrDefault(x => x.Id == id);
-
-    if (produto is null)
-    {
-        return Results.NotFound($"Produto com ID {id} não encontrado.");
-    }
-
-    produto.Nome = produtoAtualizado.Nome;
-    produto.Preco = produtoAtualizado.Preco;
-    produto.Estoque = produtoAtualizado.Estoque;
-
-    return Results.Ok(produto);
-});
-
-app.MapDelete("/produtos/{id}", (int id) =>
-{
-    var produto = produtos.FirstOrDefault(x => x.Id == id);
-
-    if (produto is null)
-    {
-        return Results.NotFound($"Produto com ID {id} não encontrado.");
-    }
-
-    produtos.Remove(produto);
-
-    return Results.NoContent();
-});
 
 app.Run();
 
